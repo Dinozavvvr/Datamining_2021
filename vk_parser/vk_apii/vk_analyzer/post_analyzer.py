fd783e7d4e10c4c51684e7142b96684d805e715d
Dinar Shagaliev <dinar.shagaliev2002@gmail.com>
2021-03-08 01:13:05 +0300

    some changes

--- vk_parser/vk_apii/vk_analyzer/post_analyzer.py
+++ vk_parser/vk_apii/vk_analyzer/post_analyzer.py
@@ -1,9 +1,4 @@
-import vk_parser.vk_apii.vk_models.post as post_py
-import vk_parser.vk_apii.vk_analyzer.analyzer_utils as util_py
-
-
 # Created by dinar at 01.03.2021
-
 
 class PostAnalyzer:
     pass
