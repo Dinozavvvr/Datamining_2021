2754a83a6d445f4193840f977e690795ab9ad8d9
Dinar Shagaliev <dinar.shagaliev2002@gmail.com>
2021-03-15 21:48:44 +0300

    some changes

--- vk_parser/dag_vk_api.py
+++ vk_parser/dag_vk_api.py
@@ -11,23 +11,23 @@ sys.path.append(str(Path('dags').absolute().parent))
 from airflow import DAG
 from main_file import start
 
-start_time = datetime.now() + timedelta(minutes=1)
+# start_time = datetime.now() + timedelta(minutes=1)
 
 
 def get_dag_default_args():
     default_args = {
         'owner': 'Dinar Shagaliev',
-        'retries': 2,
+        'retries': 1,
         'depends_on_past': False,
-        'start_date': datetime.today(),
+        'start_date': datetime(2021, 3, 12),
         'retry_delay': timedelta(minutes=1),
     }
 
     return default_args
 
 
-with DAG('vk_api', default_args=get_dag_default_args(),
-         description='post parse',schedule_interval=None) as dag:
+with DAG(dag_id='vk_api', default_args=get_dag_default_args(),
+         description='post parse', schedule_interval=None) as dag:
     vk_post_parse_task = PythonOperator(
         task_id='vk_post_parse_task',
         python_callable=start,
